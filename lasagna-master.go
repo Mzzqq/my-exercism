7e18aa5fa926a86be1614b89855483683885b885
Mzzqq <rezarifa661@gmail.com>
2024-01-19 23:27:55 +0700

    feat: Add Quantities()

--- lasagna-master.go
+++ lasagna-master.go
@@ -4,3 +4,16 @@ package main
 func PreparationTime(layers []string, time int) int {
 	return len(layers) * time
 }
+
+func Quantities(ingredient []string) (noodles int, sauce float64) {
+	noodles = 0
+	sauce = 0
+	for i, _ := range ingredient {
+		if ingredient[i] == "noodles" {
+			noodles++
+		} else if ingredient[i] == "sauce" {
+			sauce++
+		}
+	}
+	return noodles * 50, sauce * 0.2
+}
