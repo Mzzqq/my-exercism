432b7291edfa7bdc72d4602bbba52a0814ac569e
Mzzqq <rezarifa661@gmail.com>
2024-01-20 20:57:48 +0700

    feat: add InterestRate()

--- interest-is-interesting.go
+++ interest-is-interesting.go
@@ -1,1 +1,16 @@
 package main
+
+// InterestRate returns the interest rate for the provided balance.
+func InterestRate(balance float64) float32 {
+	switch {
+	case balance < 0:
+		return 3.213
+	case balance < 1000:
+		return 0.5
+	case balance < 5000:
+		return 1.621
+	case balance >= 5000:
+		return 2.475
+	}
+	return float32(balance)
+}
