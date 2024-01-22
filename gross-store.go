efaae03089a59d807762806f1b7416ec2e7b2c6e
Mzzqq <rezarifa661@gmail.com>
2024-01-22 13:19:29 +0700

    feat: add Units()

--- gross-store.go
+++ gross-store.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
-
+// Units stores the Gross Store unit measurements.
+func Units() map[string]int {
+	grossStore := map[string]int{
+		"quarter_of_a_dozen": 3,
+		"half_of_a_dozen":    6,
+		"dozen":              12,
+		"small_gross":        120,
+		"gross":              144,
+		"great_gross":        1728,
+	}
+	return grossStore
 }
