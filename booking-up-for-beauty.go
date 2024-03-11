e4d252277f40b2c70698046567da588473434761
Mzzqq <rezarifa661@gmail.com>
2024-03-11 18:12:01 +0700

    feat: add Schedule()

--- booking-up-for-beauty.go
+++ booking-up-for-beauty.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "time"
 
+// Schedule returns a time.Time from a string containing a date.
+// "7/25/2019 13:45:00"
+func Schedule(date string) time.Time {
+	t, err := time.Parse("1/02/2006 15:04:05", date)
+	if err != nil {
+		panic(err)
+	}
+	return t
 }
