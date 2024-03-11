0502291bf278d9c379715e1996fd480fa81370d2
Mzzqq <rezarifa661@gmail.com>
2024-03-11 18:52:53 +0700

    feat: Define Chessboard type in chessboard.go

--- chessboard.go
+++ chessboard.go
@@ -2,3 +2,6 @@ package main
 
 // Declare a type named File which stores if a square is occupied by a piece - this will be a slice of bools
 type File []bool
+
+// Declare a type named Chessboard which contains a map of eight Files, accessed with keys from "A" to "H"
+type Chessboard map[string]File
