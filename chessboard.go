50fcc03aa0e1b3130acbf36fe165c02bf8f7051f
Mzzqq <rezarifa661@gmail.com>
2024-03-11 18:53:14 +0700

    feat: add CountInFile()

--- chessboard.go
+++ chessboard.go
@@ -5,3 +5,15 @@ type File []bool
 
 // Declare a type named Chessboard which contains a map of eight Files, accessed with keys from "A" to "H"
 type Chessboard map[string]File
+
+// CountInFile returns how many squares are occupied in the chessboard,
+// within the given file.
+func CountInFile(cb Chessboard, file string) int {
+	count := 0
+	for _, v := range cb[file] {
+		if v {
+			count += 1
+		}
+	}
+	return count
+}
