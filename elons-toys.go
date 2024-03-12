f2b21d0d380ae4af2805afc940abcf8747f84934
Mzzqq <rezarifa661@gmail.com>
2024-03-12 09:06:37 +0700

    feat: implement method DisplayDistance()

--- elons-toys.go
+++ elons-toys.go
@@ -1,9 +1,16 @@
 package main
 
+import "fmt"
+
 // TODO: define the 'Drive()' method
 func (c *Car) Drive() {
 	if c.battery >= c.batteryDrain {
 		c.distance += c.speed
 		c.battery -= c.batteryDrain
 	}
+}
+
+// TODO: define the 'DisplayDistance() string' method
+func (c *Car) DisplayDistance() string {
+	return fmt.Sprintf("Driven %d meters", c.distance)
 }
