90108001e57df5f2b7a08529baf224a55213d3cb
Mzzqq <rezarifa661@gmail.com>
2024-03-12 09:06:19 +0700

    feat: implement method Drive()

--- elons-toys.go
+++ elons-toys.go
@@ -1,5 +1,9 @@
 package main
 
-func main() {
-
+// TODO: define the 'Drive()' method
+func (c *Car) Drive() {
+	if c.battery >= c.batteryDrain {
+		c.distance += c.speed
+		c.battery -= c.batteryDrain
+	}
 }
